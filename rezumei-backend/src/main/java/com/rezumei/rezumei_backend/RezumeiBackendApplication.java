9b62f91f9463d70c94bc1e9ef6917fb8acad94e4
KayVee03 <vyas.kartik1000@gmail.com>
2025-07-17 11:47:13 +0530

    Initial project setup for rezumei

---
+++ rezumei-backend/src/main/java/com/rezumei/rezumei_backend/RezumeiBackendApplication.java
@@ -0,0 +1,13 @@
+package com.rezumei.rezumei_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RezumeiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RezumeiBackendApplication.class, args);
+	}
+
+}
---
+++ rezumei-backend/src/test/java/com/rezumei/rezumei_backend/RezumeiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rezumei.rezumei_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RezumeiBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
